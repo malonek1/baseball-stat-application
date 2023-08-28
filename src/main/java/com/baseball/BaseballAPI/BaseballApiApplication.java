9286f9457c650b7029000c5fc00122c4b8ed99b9
malonek1 <malonek997@gmail.com>
2023-08-27 23:43:17 -0400

    removed comments

--- src/main/java/com/baseball/BaseballAPI/BaseballApiApplication.java
+++ src/main/java/com/baseball/BaseballAPI/BaseballApiApplication.java
@@ -7,14 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BaseballApiApplication {
 
-//	private static BaseballService service = new BaseballService();
-
 	public static void main(String[] args) {
 		SpringApplication.run(BaseballApiApplication.class, args);
-
-//		StandingsConsumerService standings = new StandingsConsumerService();
-//
-//		System.out.println(standings.getTeamStandings(standings.getObjectPayload()));
-
 	}
 }
